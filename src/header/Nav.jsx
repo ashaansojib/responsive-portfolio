3e1f540e5fb095c42d5f5374e1f024e9645591e0
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-08-10 23:48:24 +0600

    add nav and env of the projects

---
+++ src/header/Nav.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="my-container">
+      <div className="lg:flex justify-between items-center p-2 bg-gray-100">
+        <h2 className="text-2xl font-semibold">Ashaduzzaman Sojib</h2>
+        <div className="lg:flex items-center justify-between gap-2 lg:mt-0 mt-2">
+          <ul className="lg:flex flex-row md:justify-end items-center gap-1">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "default")}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "default")}
+              to="/about"
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "default")}
+              to="/skill"
+            >
+              Skills
+            </NavLink>
+          </ul>
+          <Link to="#">
+            <button className="py-2 px-4 inline-block font-medium hover:text-white hover:bg-green-500 rounded-sm  lg:mt-0 mt-2">
+              Download Resume
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
